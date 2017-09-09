00e12df19c8017dc400bba804c7669e585537d2c
Colin McKean <ezikill@hotmail.com>
2017-09-09 12:12:18 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,5 +1,10 @@
 def first_challenge
   epic_tragedy = {
-    montague: {},capulet:{}
+    montague: {
+      patriarch: {},
+      matriarch: {},
+      hero: {},
+      hero_friends: []
+      },capulet:{}
 }
 end
