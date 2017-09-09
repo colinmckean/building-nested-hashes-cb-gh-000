c0c01dcd677e47fa639df5b27d05dbcdaaa15e97
Colin McKean <ezikill@hotmail.com>
2017-09-09 12:15:06 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,16 +1,5 @@
 def first_challenge
   epic_tragedy = {
-    montague: {
-      patriarch: {},
-      matriarch: {},
-      hero: {},
-      hero_friends: []
-      },
-    capulet:{
-      patriarch: {},
-      matriarc: {},
-      heroine: {},
-      heroine_friends: []
-      }
-    }
-  end
+    montague: {},capulet:{}
+}
+end
